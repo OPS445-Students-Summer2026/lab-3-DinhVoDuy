63ba72baf500387e9588cfec9276e53caf261c0a
Dinh Vo <dvo12@myseneca.ca>
2026-05-28 15:40:32 -0400

    Added lab3b

---
+++ lab3b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Lab 3 Part 1 script - functions'''
+# Author ID: [seneca_id] 
+
+import sys
+
+def sum_numbers(number1, number2):
+    # Make this function add number1 and number2 and return the value
+    
+    result = number1 + number2
+    return result 
+
+def subtract_numbers(number1, number2):
+    # Make this function subtract number1 and number2 and return the value
+    # Remember to make sure the function accepts 2 arguments
+
+    result = number1 - number2
+    return result
+
+def multiply_numbers(number1, number2):
+    # Make this function multiply number1 and number2 and return the value
+    # Remember to make sure the function accepts 2 arguments
+    
+    result = number1 * number2
+    return result
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
